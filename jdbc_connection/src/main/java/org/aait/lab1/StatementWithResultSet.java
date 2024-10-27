5f47cbfacecbc12ae11a979654c40cc7708c04c2
EstifanosTaye320 <estifanostaye11@gmail.com>
2024-10-27 14:23:23 +0300

    Reading teachers table complete (select query)

--- jdbc_connection/src/main/java/org/aait/lab1/StatementWithResultSet.java
+++ jdbc_connection/src/main/java/org/aait/lab1/StatementWithResultSet.java
@@ -2,8 +2,10 @@ package org.aait.lab1;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
 
 public class StatementWithResultSet {
 
@@ -26,7 +28,13 @@ public class StatementWithResultSet {
             "insert into teachers(first_name, last_name, school, hire_date, salary) values('Aster', 'Nega', 'Yekatit 12', '2021-01-01', 8000)",
             "insert into teachers(first_name, last_name, school, hire_date, salary) values('Jemal', 'Edris', 'Bole', '2021-09-11', 20000)",
             "insert into teachers(first_name, last_name, school, hire_date, salary) values('Haile', 'Anaol', 'Saris', '2022-01-22', 15000)",
-            "insert into teachers(first_name, last_name, school, hire_date, salary) values('Teddy', 'Anbesaw', 'Bole', '2021-01-01', 8000)",};
+            "insert into teachers(first_name, last_name, school, hire_date, salary) values('Teddy', 'Anbesaw', 'Bole', '2021-01-01', 15000)",
+            "insert into teachers(first_name, last_name, school, hire_date, salary) values('Haile', 'Anbesaw', 'Bole', '2021-01-01', 15000)",
+            "insert into teachers(first_name, last_name, school, hire_date, salary) values('Teklay', 'Anbesaw', 'Bole', '2021-01-01', 6000)",
+            "insert into teachers(first_name, last_name, school, hire_date, salary) values('Johny', 'Anbesaw', 'Bole', '2021-01-01', 40000)",
+            "insert into teachers(first_name, last_name, school, hire_date, salary) values('Memar', 'Anbesaw', 'Bole', '2021-01-01', 33000)",
+            "insert into teachers(first_name, last_name, school, hire_date, salary) values('Memar', Null, 'Bole', Null, 5000)",};
+        String selectQuery = "select * from teachers";
 
         try (Connection connection = DriverManager.getConnection(url, username, password); Statement statement = connection.createStatement()) {
             statement.executeUpdate(createDatabaseQuery);
@@ -35,7 +43,7 @@ public class StatementWithResultSet {
             System.err.println(e);
         }
 
-        try (Connection connection = DriverManager.getConnection(url + databaseName, username, password); Statement statement = connection.createStatement()) {
+        try (Connection connection = DriverManager.getConnection(url + databaseName, username, password); Statement statement = connection.createStatement();) {
             statement.executeUpdate(createTableQuery);
             System.out.println("Table " + tableName + " created!");
 
@@ -43,6 +51,26 @@ public class StatementWithResultSet {
                 statement.executeUpdate(stmt);
             }
             System.out.println("Data Inserted Successfullly.");
+
+        } catch (SQLException e) {
+            System.err.println(e);
+        }
+
+        try (Connection connection = DriverManager.getConnection(url + databaseName, username, password); Statement statement = connection.createStatement(); ResultSet resultset = statement.executeQuery(selectQuery)) {
+            while (resultset.next()) {
+                int id = resultset.getInt("id");
+                String fname = resultset.getString("first_name");
+                Date hire_date = resultset.getDate("hire_date");
+                double salary = resultset.getDouble("salary");
+
+                System.out.println("Teacher ID: " + id);
+                System.out.println("Teacher Name: " + fname);
+                System.out.println("Teacher Hire Date: " + hire_date);
+                System.out.println("Teacher Salary: " + salary);
+                System.out.println("...............");
+            }
+
+            System.out.println("Reading teachers table complete!");
         } catch (SQLException e) {
             System.err.println(e);
         }
