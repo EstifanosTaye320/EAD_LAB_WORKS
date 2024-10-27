e0a36842c49f6aea6c094c0674d14c2bd4b19b07
EstifanosTaye320 <estifanostaye11@gmail.com>
2024-10-27 13:15:08 +0300

    Remove password from code

--- jdbc_connection/src/main/java/org/aait/lab1/CreateDatabaseExample.java
+++ jdbc_connection/src/main/java/org/aait/lab1/CreateDatabaseExample.java
@@ -10,7 +10,7 @@ public class CreateDatabaseExample {
     public static void main(String[] args) {
         String url = "jdbc:mysql://localhost:3306/";
         String username = "root";
-        String password = "Remember2020!!!!!";
+        String password = "my_password";
         String databaseName = "StudentDB";
 
         try (Connection connection = DriverManager.getConnection(url, username, password); Statement statement = connection.createStatement()) {
--- jdbc_connection/src/main/java/org/aait/lab1/JDBCDemo.java
+++ jdbc_connection/src/main/java/org/aait/lab1/JDBCDemo.java
@@ -8,16 +8,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class JDBCDemo {
+
     public static void main(String[] args) {
-        try {
-            String driver = "com.mysql.cj.jdbc.Driver";
-            String url = "jdbc:mysql://localhost:3306/church_db";
-            String username = "root";
-            String password = "my_password";
+        String driver = "com.mysql.cj.jdbc.Driver";
+        String url = "jdbc:mysql://localhost:3306/church_db";
+        String username = "root";
+        String password = "my_password";
+        try (Connection conn = DriverManager.getConnection(url, username, password);) {
             Class.forName(driver);
-            Connection conn = DriverManager.getConnection(url, username, password);
+
             System.out.println("Established Connection");
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.err.println(e);
         }
     }
