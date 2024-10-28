a51cb4e60d8737eff563de9320eebbcc12ae37d7
Estifanos Taye <estifanostaye11@gmail.com>
2024-10-28 10:13:52 +0300

    setting up practice area

--- jdbc_connection/src/main/java/org/aait/lab1/JDBCDemo.java
+++ jdbc_connection/src/main/java/org/aait/lab1/JDBCDemo.java
@@ -10,15 +10,12 @@ import java.sql.DriverManager;
 public class JDBCDemo {
 
     public static void main(String[] args) {
-        String driver = "com.mysql.cj.jdbc.Driver";
         String url = "jdbc:mysql://localhost:3306/church_db";
         String username = "root";
         String password = "my_password";
 
-        try (Connection conn = DriverManager.getConnection(url, username, password);) {
-            Class.forName(driver);
-
-            System.out.println("Established Connection");
+        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+            System.out.println("Connection Established");
         } catch (Exception e) {
             System.err.println(e);
         }
